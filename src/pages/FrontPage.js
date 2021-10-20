bff7bd39214ce93aea78f4ecbc5cd6a4f0ab90c7
Anushree Das <64682931+meAnushree@users.noreply.github.com>
2021-10-20 22:37:39 +0530

    Complete Navbar

---
+++ src/pages/FrontPage.js
@@ -0,0 +1,15 @@
+import Navbar from "../components/Navbar"
+import classes from "./FrontPage.module.css"
+
+
+
+const FrontPage = () => {
+  return (
+   <main>
+     <Navbar/>
+     
+   </main>
+  )
+}
+
+export default FrontPage
