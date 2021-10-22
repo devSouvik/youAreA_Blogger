4f5d7dee042917411e523b21d5f7285c8e62a2b3
Anushree Das <64682931+meAnushree@users.noreply.github.com>
2021-10-22 18:05:04 +0530

    Complete FrontPageCard

---
+++ src/components/FrontPageCard.js
@@ -0,0 +1,23 @@
+import classes from './FrontPageCard.module.css'
+import person from "../assets/images/person.jpg"
+
+
+const FrontPageCard = (props) => {
+    return (
+        <div className={classes.main}>
+           <div className={classes.index}></div> 
+           <div className={classes.body}>
+               <div className={classes["body__header"]}>
+                   <div className={classes.imagediv}>
+                       {/* <img src={person} alt="person" className={classes.image} /> */}
+                       </div>
+                   <h5 className={classes['header__title']}>{props.author}</h5>
+               </div>
+               <h3 className={classes["body__main"]}>{props.title}</h3>
+                  <p className={classes.subtitle}>{props.date}. {props.readTime} read</p>
+           </div>
+        </div>
+    )
+}
+
+export default FrontPageCard
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,24 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import classes from "./Header.module.css";
-import image from "../assets/images/logos/small-logo.png"
+import image from "../assets/images/logos/small-logo.png";
 
 const Header = () => {
-    return (
-        <div className={classes.main}>
-            <div className={classes.left}>
-                <h1>Blogger is a place to write, read and connect.</h1>
-                <h4>It's easy and free to post your thinking on any topic and connect with millons of readers.</h4>
-                <a href="#" className={classes.startButton}>
-                   Start Writing </a>
-            </div>
-            <img className={classes["small-logo"]} src={image} />
-        </div>
-    )
-}
+  return (
+    <div className={classes.main}>
+      <div className={classes.left}>
+        <h1>Blogger is a place to write, read and connect.</h1>
+        <h4>
+          It's easy and free to post your thinking on any topic and connect with
+          millons of readers.
+        </h4>
+        <a  href="#" className={classes.startButton}>
+          Start Writing{" "}
+        </a>
+      </div>
+      <div></div>
+      <img className={classes["small-logo"]} src={image} alt="brand-logo"/>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
