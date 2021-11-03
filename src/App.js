26653e20418d3201de5ac6639a208d3456622fba
Anushree Das <64682931+meAnushree@users.noreply.github.com>
2021-11-03 21:37:39 +0530

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import logo from './logo.svg';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
 import './App.css';
 import FrontPage from './pages/FrontPage';
 
 function App() {
   return (
-    <FrontPage />
-    
-
-  );
-}
-
+    <Router>
+      <Switch>
+         <Route path="/" exact>
+           <FrontPage/>
+         </Route>
+         <Route path="*">
+            <h1>Page not found</h1>
+         </Route>
+      </Switch>
+    </Router>
+  )
+};
 export default App;
+
