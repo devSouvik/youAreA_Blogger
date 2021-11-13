b95210c79c652bf8cb94ccfd458bde9d7e6ab355
Anushree Das <64682931+meAnushree@users.noreply.github.com>
2021-11-13 22:11:37 +0530

    add profile header

---
+++ src/components/Profile_Page/ProfileBody.js
@@ -0,0 +1,16 @@
+import classes from "./ProfileBody.module.css"
+import ProfileHeader from "./ProfileHeader"
+
+
+const ProfileBody = () => {
+    return (
+        <div className={classes.main}>
+        
+        <ProfileHeader />
+    
+       
+        </div>
+    )
+}
+
+export default ProfileBody
--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,9 +1,12 @@
 import Navbar from "../components/common/DefaultNavbar";
+import ProfileBody from "../components/Profile_Page/ProfileBody";
+
 
 const ProfilePage = () => {
     return (
         <div>
             <Navbar />
+            <ProfileBody />
             
         </div>
     )
