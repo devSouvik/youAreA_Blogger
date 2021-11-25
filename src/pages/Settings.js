54c90e8e3d3d8d8bcb117312e8f7f2019ac72f0d
devSouvik <souvikguria98@gmail.com>
2021-11-25 22:42:16 +0530

    settings page with security tab added

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,19 +1,14 @@
 import DefaultNavbar from "../components/common/DefaultNavbar";
-import Left_col from "../components/Settings_Page/Left_col";
-import Right_col from "../components/Settings_Page/Right_col";
+import Right_col from "../components/Settings_Page/Settings_col";
 import "./Settings.css";
 
 export default function Settings() {
   return (
     <div>
       <DefaultNavbar />
+
       <div class="settings_column row ">
-        <div class="col-4">
-          <Left_col />
-        </div>
-        <div class=" col-lg-8 col-md-12 col-xs-12 col-12">
-          <Right_col />
-        </div>
+        <Right_col />
       </div>
     </div>
   );
