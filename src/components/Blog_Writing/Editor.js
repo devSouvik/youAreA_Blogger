ea2af47833cb62d4ba1574351bbb108a68ff8d85
devSouvik <souvikguria98@gmail.com>
2021-12-12 07:57:18 +0530

    blog-write page  updated with a new heading box

--- src/components/Blog_Writing/Editor.js
+++ src/components/Blog_Writing/Editor.js
@@ -3,29 +3,70 @@ import "./Editor.css";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import parse from "html-react-parser";
+// mui imports
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import { makeStyles, StylesProvider } from "@mui/styles";
+
+const useStyles = makeStyles({
+  helperText: {
+    fontWeight: "bold",
+    fontSize: 15,
+  },
+  labelText: {
+    color: "black",
+    fontWeight: "bold",
+  },
+});
 
 export default function Editor() {
+  const [heading, setHeading] = useState("");
   const [articleData, setArticleData] = useState("");
   const [showPost, setshowPost] = useState(false);
 
   const finaldata = parse(articleData);
 
   const handleChange = (event, editor) => {
-    const data = editor.getData();
+    let data = editor.getData();
     setArticleData(data);
   };
 
   const postSubmit = () => {};
 
+  const classes = useStyles();
+
   return (
     <div className="container">
+      {/* <h1 className="articleHeading">Story Title</h1> */}
+
       <h1 className="articleHeading">Write your story here . . .</h1>
-      <div className="py-3">
+      <Box
+        sx={{
+          width: 500,
+          maxWidth: "100%",
+        }}
+      >
+        <TextField
+          fullWidth
+          label="Story Title"
+          id="storyTitle"
+          InputLabelProps={{
+            className: classes.labelText,
+          }}
+          FormHelperTextProps={{
+            className: classes.helperText,
+          }}
+          onChange={(e) => {
+            setHeading(e.target.value);
+          }}
+        />
+      </Box>
+      <div style={{ marginTop: 15 }}>
         <CKEditor editor={ClassicEditor} onChange={handleChange} />
       </div>
       <hr />
 
-      <button className="btn btn-dark" onClick={() => setshowPost(!showPost)}>
+      <button className="btn" onClick={() => setshowPost(!showPost)}>
         Toggle Article Preview
       </button>
 
