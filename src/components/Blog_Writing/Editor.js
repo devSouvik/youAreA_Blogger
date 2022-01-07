d6f11130645eec71c7e356478ce0be4d31f6b1a3
devSouvik <souvikguria98@gmail.com>
2022-01-07 23:23:46 +0530

    write post done

--- src/components/Blog_Writing/Editor.js
+++ src/components/Blog_Writing/Editor.js
@@ -1,78 +1,100 @@
-import React, { useState } from "react";
+import React, { useRef } from "react";
 import "./Editor.css";
-import { CKEditor } from "@ckeditor/ckeditor5-react";
-import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
-import parse from "html-react-parser";
-// mui imports
-import Box from "@mui/material/Box";
-import TextField from "@mui/material/TextField";
-import { makeStyles } from "@mui/styles";
+import JoditEditor from "jodit-react";
+// import { useRef } from "react";
 
-const useStyles = makeStyles({
-  helperText: {
-    fontWeight: "bold",
-    fontSize: 15,
-  },
-  labelText: {
-    color: "black",
-    fontWeight: "bold",
-  },
-});
-
-export default function Editor() {
-  const [heading, setHeading] = useState("");
-  const [articleData, setArticleData] = useState("");
-  const [showPost, setshowPost] = useState(false);
-
-  const finaldata = parse(articleData);
-
-  const handleChange = (event, editor) => {
-    let data = editor.getData();
-    setArticleData(data);
+export default function Editor({ postText }) {
+  const editor = useRef(null);
+  const config = {
+    controls: {
+      font: {
+        list: {
+          "Roboto Medium,Arial,sans-serif": "Roboto",
+          "charter, Georgia, Cambria, Times new Roman ,Times, serif": "charter",
+        },
+      },
+    },
   };
 
-  // use this function for submitting posts
-  // const postSubmit = () => {};
-
-  const classes = useStyles();
-
   return (
-    <div className="container">
-      {/* <h1 className="articleHeading">Story Title</h1> */}
-
-      <h1 className="articleHeading">Write your story here . . .</h1>
-      <Box
-        sx={{
-          width: 500,
-          maxWidth: "100%",
+    <div>
+      <JoditEditor
+        ref={editor}
+        config={config}
+        tabIndex={1} // tabIndex of textarea
+        onChange={(content) => {
+          postText.current = content;
         }}
-      >
-        <TextField
-          fullWidth
-          label="Story Title"
-          id="storyTitle"
-          InputLabelProps={{
-            className: classes.labelText,
-          }}
-          FormHelperTextProps={{
-            className: classes.helperText,
-          }}
-          onChange={(e) => {
-            setHeading(e.target.value);
-            console.log(heading);
-          }}
-        />
-      </Box>
-      <div style={{ marginTop: 15 }}>
-        <CKEditor editor={ClassicEditor} onChange={handleChange} />
-      </div>
-      <hr />
-
-      <button className="btn" onClick={() => setshowPost(!showPost)}>
-        Toggle Article Preview
-      </button>
-
-      <div className="container px-5 py-5">{showPost && finaldata}</div>
+      />
     </div>
   );
 }
+
+// const useStyles = makeStyles({
+//   helperText: {
+//     fontWeight: "bold",
+//     fontSize: 15,
+//   },
+//   labelText: {
+//     color: "black",
+//     fontWeight: "bold",
+//   },
+// });
+
+// export default function Editor() {
+//   const [heading, setHeading] = useState("");
+//   const [articleData, setArticleData] = useState("");
+//   const [showPost, setshowPost] = useState(false);
+
+//   const finaldata = parse(articleData);
+
+//   const handleChange = (event, editor) => {
+//     let data = editor.getData();
+//     setArticleData(data);
+//   };
+
+//   // use this function for submitting posts
+//   // const postSubmit = () => {};
+
+//   const classes = useStyles();
+
+//   return (
+//     <div className="container">
+//       {/* <h1 className="articleHeading">Story Title</h1> */}
+
+//       <h1 className="articleHeading">Write your story here . . .</h1>
+//       <Box
+//         sx={{
+//           width: 500,
+//           maxWidth: "100%",
+//         }}
+//       >
+//         <TextField
+//           fullWidth
+//           label="Story Title"
+//           id="storyTitle"
+//           InputLabelProps={{
+//             className: classes.labelText,
+//           }}
+//           FormHelperTextProps={{
+//             className: classes.helperText,
+//           }}
+//           onChange={(e) => {
+//             setHeading(e.target.value);
+//             console.log(heading);
+//           }}
+//         />
+//       </Box>
+//       <div style={{ marginTop: 15 }}>
+//         <CKEditor editor={ClassicEditor} onChange={handleChange} />
+//       </div>
+//       <hr />
+
+//       <button className="btn" onClick={() => setshowPost(!showPost)}>
+//         Toggle Article Preview
+//       </button>
+
+//       <div className="container px-5 py-5">{showPost && finaldata}</div>
+//     </div>
+//   );
+// }
--- src/pages/Blog_writing.js
+++ src/pages/Blog_writing.js
@@ -1,11 +1,86 @@
 import Editor from "../components/Blog_Writing/Editor";
 import DefaultNavbar from "../components/common/DefaultNavbar";
+import { useState, useRef, useContext } from "react";
+import { addDoc, collection } from "firebase/firestore";
+import { auth, db } from "../firebase";
+import { useNavigate } from "react-router-dom";
+import { Button } from "@mui/material";
+import { GlobalContext } from "../contexts/GlobalContext";
+
+// mui imports
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import { makeStyles } from "@mui/styles";
+// import { createTheme } from "@mui/material/styles";
+
+const useStyles = makeStyles({
+  helperText: {
+    fontWeight: "bold",
+    fontSize: 15,
+  },
+  labelText: {
+    color: "black",
+    fontWeight: "bold",
+  },
+});
 
 export default function Blog_writing() {
+  // const [postText, setPostText] = useState("");
+  const [title, setTitle] = useState("");
+  let navigate = useNavigate();
+  const postText = useRef("");
+  const { user } = useContext(GlobalContext);
+
+  const postsCollectionRef = collection(db, "posts");
+  const createPost = async () => {
+    await addDoc(postsCollectionRef, {
+      title,
+      postText: postText.current,
+      author: { name: user.username, id: user.id },
+    });
+    navigate("/home");
+  };
+
+  const classes = useStyles();
+
   return (
     <div>
       <DefaultNavbar />
-      <Editor />
+      <div className="container">
+        <h1>Let's write something . . .</h1>
+        <Box
+          sx={{
+            width: 500,
+            maxWidth: "100%",
+          }}
+        >
+          <TextField
+            fullWidth
+            label="Post Title"
+            id="storyTitle"
+            InputLabelProps={{
+              className: classes.labelText,
+            }}
+            FormHelperTextProps={{
+              className: classes.helperText,
+            }}
+            onChange={(event) => {
+              setTitle(event.target.value);
+              console.log(title);
+            }}
+          />
+        </Box>
+        <div style={{ marginTop: 15 }}>
+          <Editor postText={postText} />
+        </div>
+      </div>
+      <Button
+        variant="contained"
+        style={{ float: "right", marginRight: "5%", marginTop: 20 }}
+        onClick={createPost}
+      >
+        Submit Post
+      </Button>
     </div>
   );
 }
